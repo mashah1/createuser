8ccc303d0f43423d199617aca94c19a8cc6cad5f
csamp <csamp@aspirago.com>
2016-02-11 14:15:34 -0500

    Changed default messages

--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -31,6 +31,7 @@ module.exports = function (config) {
             'main/webapp/bower_components/angular-ui-router/release/angular-ui-router.js',
             'main/webapp/bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
             'main/webapp/bower_components/json3/lib/json3.js',
+            'main/webapp/bower_components/kendo-ui/js/kendo.ui.core.min.js',
             'main/webapp/bower_components/ng-file-upload/ng-file-upload.js',
             'main/webapp/bower_components/ngInfiniteScroll/build/ng-infinite-scroll.js',
             'main/webapp/bower_components/angular-mocks/angular-mocks.js',
